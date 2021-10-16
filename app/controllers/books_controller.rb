2ff6869ad5e656277642edbc503f5ab863387528
massu0819 <massu4869@gmail.com>
2021-10-16 08:02:22 +0000

    [Finish]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,9 +36,10 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
+    end
   end
 
   private
